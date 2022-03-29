c9305f660d42201add3408e0752e25b7a797bffa
BismuthBetel <97366538+BismuthBetel@users.noreply.github.com>
2022-03-29 01:09:06 -0500

    updated HTML file and commented JS file, updated README file

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # CodeQuizRepo
+
+## Purpose
+
+This application is a timed quiz based on code related questions. The timer will countdown as the quiz is taken. If an incorrect answer is selected, time will be subtracted from the timer as penalty.
---
+++ assets/js/file.js
@@ -0,0 +1,27 @@
+//global values
+
+
+
+//element references
+
+
+
+//start button functionality
+
+//questions and answer handling
+
+
+//buttons for answers functionality
+
+//Timeinterval/setTime function for timer
+
+
+//Penalty subtraction of time from timer
+
+//function for saving the score and initials
+
+// final submit button for saving score and initials
+
+//endGame function
+
+//event Listeners
